52052e27a33cb48c4ad5079f6acec8eb2b875112
vivekkedia22 <vivekkedia22@gmail.com>
2026-01-12 03:06:01 +0530

    add:log for the db connection error

--- Server/src/db/index.ts
+++ Server/src/db/index.ts
@@ -4,6 +4,7 @@ export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI!);
   } catch (error) {
+    console.log("Connection error", error)
     throw new Error("Database connection failed");
   }
 };
